ef2d344a8cc0f5eba9a42b82367b8e7e628ae8a4
Arunwolf-tech <mammapaiya1987@gmail.com>
2025-07-10 16:56:28 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+window.addEventListener("scroll",function(){
+
+    document.querySelector("header"). classList.toggle("sticky",window.scrollY > 0);
+});
+
+
+
+
+const menu = document.querySelector('#menu')
+const navbar = document.querySelector('.navbar')
+
+window.onload = () => {
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar')
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    navbar.classList.toggle('open');
+    
+};
+};
+
+
+
+
+
+const sr=ScrollReveal ({
+    distance: '30px',
+    duration: 2600,
+    reset: true
+})
+
+sr.reveal('.home-text',{delay:280, origin:'bottom'})
+
+sr.reveal('.middle-text,.Contact',{delay:200, origin:'bottom'})
